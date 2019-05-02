b16462b00349e6c9283bde289cc68a3179b86841
beh70 <43176752+beh70@users.noreply.github.com>
2019-05-02 15:25:34 -0400

    Update Dragon.java

--- Dragon.java
+++ Dragon.java
@@ -1,4 +1,31 @@
 public class Dragon extends Person{
-
+	
+	private int health;
+	private int damage;
+	private int defense;
+	  
+	
   public Dragon(int health, int damage, int defense) { 
-		
+	
+	this.health = health; 
+	this.damage = damage;
+	this.defense = defense; 
+  }
+	public void setHealth(int health){
+		this.health = health; 
+	}
+	public void setDamage(int damage){
+		this.damage = damage;
+	}
+	public void setDefense(int defense){
+		this.defense = defense;
+	}
+	public int getHealth(){
+		return this.health;
+	}
+	public int getDamage(){
+		return this.damage;
+	}
+	public int getDefense(){
+		return this.defense;
+	}
