87f05d9a3a0cd9865ee396d94ce4caf7ddcc7e0c
mikesudol123 <49042503+mikesudol123@users.noreply.github.com>
2019-05-02 15:35:26 -0400

    Update Ants.java

--- Ants.java
+++ Ants.java
@@ -4,4 +4,41 @@ public class Ants extends Person{
   private int defense;
   private int health;
   
+  public Ants(int damage) {
+    this.damage = damage;
+  }
   
+  public int getDamage() {
+    return damage;
+  }
+  
+  public void setDamage(int damage) {
+    this.damage = damage;
+  }//end of damage
+  
+  public Ants(int defense) {
+    this.defense = defense;
+  }
+  
+  public int getDefense() {
+    return defense;
+  }
+  
+  public void setDefense(int defense) {
+    this.defense = defense;
+  }//end of defense
+  
+  public Ants(int health) {
+    this.health = health;
+  }
+  
+  public int getHealth() {
+    return health;
+  }
+  
+  public void setHealth(int health) {
+    this.health = health;
+  }//end of health 
+  
+}
+
