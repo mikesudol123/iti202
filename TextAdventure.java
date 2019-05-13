7a4db4c7054ff65e810e34cf7284e5e748285c40
beh70 <43176752+beh70@users.noreply.github.com>
2019-05-13 19:55:31 -0400

    Update TextAdventure.java

--- TextAdventure.java
+++ TextAdventure.java
@@ -1,23 +1,22 @@
+
 import java.util.Scanner;
 
 
 public class TextAdventure extends ActualGame{
 
 
-
 	public static void main(String[] args) {
-
+			//Beginning text of game.
 			System.out.println("Welcome to The Hunt for The Blade of Akash.\r\n" + "Move along into the rooms searching for The Blade of Akash.\r\n" +
 				"Collect Armor, Health, and Defense stats.\r\n" +
 				"Once ye found The Blade of Akash, defeat the Dragon.");
 		System.out.println("Please enter yer name.");
+		//Use scanner to reader user input for the rest of the game.
 		Scanner input = new Scanner(System.in);
 		String name = input.nextLine();
 
 		System.out.println("Welcome Knight " + name + ".");
 		ActualGame game = new ActualGame();
-
-
 
 
 		game.populateRooms();
@@ -26,7 +25,7 @@ public class TextAdventure extends ActualGame{
 		String nav;
 		int x;
 
-
+		//Starting position of game.
 		System.out.println("Ye are in an empty room...");
 			System.out.println("Where would yeh like to go, please enter a cardinal directon.");
 
